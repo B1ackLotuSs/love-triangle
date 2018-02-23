4ee5a1b07d48b88525df6529502a9d81b84ba8f3
B1ackLotuSs <nikita_chernukhin@mail.ru>
2018-02-23 22:49:40 +0300

    ChernukhinTest2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  // your implementation'
+  
+    var z=null ;
+     var n = preferences.length;
+    preferences.unshift('10000');
+      for(var i=1; i<=n; i++)  { 
+          for(var j=1; j<=n; j++)   { 
+              var c = preferences[i] ;
+              if ((c == j) && !(i == j))  {
+                  for(var k=1; k<=n; k++) {
+                      var d = preferences[j];
+                      var g = preferences[k];
+                  if ((d == k) && (g == i))  {
+                           z=z+1 ; 
+                  }
+                      }
+                  } 
+              } 
+              
+          } return  z/3  ;
 };
